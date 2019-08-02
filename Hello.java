ccb978a11c0b0da67c3770b741becb0e64693f0d
BYUNGSOO <eedock92@naver.com>
2019-08-02 12:34:34 +0900

    hellojava

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package helloProject; //package는 자바의 예약어로, 소스 파일을 다양한 기준으로 분리해서 관리.
+
+public class Hello { //public, class 는 자바 에약어 public 이 붙은 class의 이름은 반드시 파일 이름과 같아야함
+	/*
+	 * 여는 중괄호 {, class 영역의 시작을 나타태며, 짝을 이룬다.) 클래스 영역. 클래스 영역에 뒤어 배울 , 맴버 변수, 멤버 메서드
+	 * 등 선언문 위치.
+	 */
+	public static void main(String[] args) {//메인 메서드 내부 중괄호 내부를 메서드 구현부라고 한다, 어떤 일을 수행하는 실행문이 작성되는 곳
+		
+		System.out.println("안녕 자바!!");
+
+	}
+
+}
